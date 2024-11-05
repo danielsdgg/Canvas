b9fba481816c90570d057867477241a449be7e49
Daniel Muiruri <gathigidg26@gmail.com>
2024-11-05 13:51:37 +0300

    :fire:Changing stack - using django for more scalability

--- backend/config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://lms_user:your_password_here@localhost/lms_db')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    JWT_SECRET_KEY = 'your_jwt_secret_key'  # Change this to a random secret
