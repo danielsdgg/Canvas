07b5e67b4dfefcf5b6277d7e0db01e3a422c835f
Daniel Muiruri <gathigidg26@gmail.com>
2025-02-19 12:23:26 +0300

    :construction:Fetching students List successfully - an admin can see their class list of students and manage them

--- frontend/src/pages/Admin/Admindashboard.tsx
+++ frontend/src/pages/Admin/Admindashboard.tsx
@@ -1,82 +1,44 @@
 import React, { useEffect, useState } from 'react';
-import { NavLink, useNavigate } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import { useAuth } from '../../context/authContext';
 import SideNav from '../../components/SideNav';
 
-interface User {
+interface Student {
   id: number;
   emailAddress: string;
   firstName: string;
   lastName: string;
+  role: string;
   phoneNumber: string | null;
   createdAt: string;
-  role: string;
-  managedBy: string | null;
-  courses: any[];
-}
-
-interface Course {
-  id: number;
-  courseName: string;
 }
 
 const Admindashboard: React.FC = () => {
-  const [users, setUsers] = useState<User[]>([]);
-  const [courses, setCourses] = useState<Course[]>([]);
+  const [students, setStudents] = useState<Student[]>([]);
   const [loading, setLoading] = useState<boolean>(true);
-  const [selectedUser, setSelectedUser] = useState<number | null>(null);
-  const [selectedCourse, setSelectedCourse] = useState<number | null>(null);
-  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
   const [error, setError] = useState<string | null>(null);
-  const navigate = useNavigate();
   const { userToken } = useAuth();
+  const adminId = 1;
+  const courseId = 1;
 
   useEffect(() => {
-    if (!userToken) {
-      console.error('User token is missing.');
-      navigate('/login');
-    } else {
-      fetchUsers();
-      fetchCourses();
-    }
-  }, [userToken, navigate]);
+    fetchStudents();
+  }, [userToken]);
 
-  const fetchUsers = async () => {
+  const fetchStudents = async () => {
     try {
-      const response = await fetch('/api/v1/users', {
+      const response = await fetch(`/api/v1/users/students?adminId=${adminId}&courseId=${courseId}`, {
         headers: {
           Authorization: `Bearer ${userToken}`,
         },
       });
-
-      if (!response.ok) {
-        console.error('Failed to fetch users.');
-        return;
-      }
-
-      const data = await response.json();
-      setUsers(Array.isArray(data) ? data : []);
-    } catch (error) {
-      console.error('Error fetching users:', error);
-    } finally {
-      setLoading(false);
-    }
-  };
-
-  const fetchCourses = async () => {
-    try {
-      const response = await fetch("/api/v1/courses/", {
-        headers: {
-          Authorization: `Bearer ${userToken}`, // Use the token from context in the headers
-        },
-      });
-
+      
       if (!response.ok) {
         throw new Error(`HTTP error! status: ${response.status}`);
       }
-
+      
       const data = await response.json();
-      setCourses(data);
+      setStudents(Array.isArray(data) ? data : []);
     } catch (err: any) {
       setError(err.message);
     } finally {
@@ -84,35 +46,8 @@ const Admindashboard: React.FC = () => {
     }
   };
 
-  const handleEnrollClick = (userId: number) => {
-    setSelectedUser(userId);
-    setIsModalOpen(true);
-  };
-
-  const enrollStudent = async () => {
-    if (!selectedUser || !selectedCourse) return;
-    try {
-      const response = await fetch(`/api/v1/courses/${selectedCourse}/enroll/${selectedUser}`, {
-        method: 'POST',
-        headers: {
-          Authorization: `Bearer ${userToken}`,
-          'Content-Type': 'application/json',
-        },
-      });
-      if (response.ok) {
-        alert('Student successfully enrolled!');
-        fetchUsers(); 
-      } else {
-        alert('Failed to enroll student.');
-      }
-    } catch (error) {
-      console.error('Error enrolling student:', error);
-    }
-    setIsModalOpen(false);
-  };
-
   if (loading) {
-    return <p className="text-center text-lg text-gray-700">Loading users...</p>;
+    return <p className="text-center text-lg text-gray-700">Loading students...</p>;
   }
 
   return (
@@ -122,7 +57,7 @@ const Admindashboard: React.FC = () => {
         <div className="flex-1 p-5">
           <header className="text-3xl text-center font-bold text-blue-700 mb-5">ADMIN DASHBOARD</header>
           <div className="bg-white shadow-lg rounded-lg p-4">
-            <h2 className="text-2xl font-bold mb-4 text-red-600">Students List</h2>
+            <h2 className="text-2xl font-bold mb-4 text-red-600">Students Class List</h2>
             <div className="overflow-auto">
               <table className="min-w-full text-left text-sm sm:text-base">
                 <thead>
@@ -132,34 +67,24 @@ const Admindashboard: React.FC = () => {
                     <th className="px-4 py-2">Email</th>
                     <th className="px-4 py-2">Phone Number</th>
                     <th className="px-4 py-2">Joined</th>
-                    <th className="px-4 py-2">Course Enrolled</th>
-                    <th className="px-4 py-2">Instructor</th>
+                    <th className="px-4 py-2">Role</th>
+
                   </tr>
                 </thead>
                 <tbody>
-                  {users.filter(user => user.role === 'STUDENT').map(user => (
-                    <tr key={user.id} className="hover:bg-blue-200">
-                      <NavLink to={`/user/${user.id}`} className="text-blue-500 hover:text-blue-700">
-                          {user.firstName}
-                        </NavLink>
-                      <td className="px-4 py-2">{user.lastName}</td>
-                      <td className="px-4 py-2">{user.emailAddress}</td>
-                      <td className="px-4 py-2">{user.phoneNumber || 'N/A'}</td>
-                      <td className="px-4 py-2">{new Date(user.createdAt).toLocaleDateString()}</td>
+                  {students.map(student => (
+                    <tr key={student.id} className="hover:bg-blue-200">
                       <td className="px-4 py-2">
-            
-                        {user.courses.length > 0 ? user.courses.map(course => course.courseName).join(', ') : (
-                          <>
-                            <span className="italic">Not enrolled</span>
-                            <button 
-                              onClick={() => handleEnrollClick(user.id)}
-                              className="ml-3 bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-3 rounded">
-                              Enroll
-                            </button>
-                          </>
-                        )}
+                        <NavLink to={`/user/${student.id}`} className="text-blue-500 hover:text-blue-700">
+                          {student.firstName}
+                        </NavLink>
                       </td>
-                      <td className="px-4 py-2">{user.managedBy}</td>
+                      <td className="px-4 py-2">{student.lastName}</td>
+                      <td className="px-4 py-2">{student.emailAddress}</td>
+                      <td className="px-4 py-2">{student.phoneNumber || 'N/A'}</td>
+                      <td className="px-4 py-2">{new Date(student.createdAt).toLocaleDateString()}</td>
+                      <td className="px-4 py-2">{student.role}</td>
+
                     </tr>
                   ))}
                 </tbody>
@@ -168,24 +93,6 @@ const Admindashboard: React.FC = () => {
           </div>
         </div>
       </div>
-
-      {isModalOpen && (
-        <div className="fixed inset-0 flex items-center justify-center bg-gray-800 bg-opacity-50">
-          <div className="bg-white p-5 rounded shadow-lg">
-            <h2 className="text-xl font-bold mb-3">Select a Course</h2>
-            <select onChange={(e) => setSelectedCourse(Number(e.target.value))} className="p-2 border rounded w-full">
-              <option value="">-- Select a Course --</option>
-              {courses.map(course => (
-                <option key={course.id} value={course.id}>{course.courseName}</option>
-              ))}
-            </select>
-            <div className="mt-4">
-              <button onClick={enrollStudent} className="bg-green-500 text-white px-4 py-2 rounded mr-2">Enroll</button>
-              <button onClick={() => setIsModalOpen(false)} className="bg-red-500 text-white px-4 py-2 rounded">Cancel</button>
-            </div>
-          </div>
-        </div>
-      )}
     </>
   );
 };
