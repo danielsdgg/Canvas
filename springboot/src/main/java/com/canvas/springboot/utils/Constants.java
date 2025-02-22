9f5981da32814a8fb181555211af1a4516c9cd1b
Ngare Maina <45742522+Ngaremaina@users.noreply.github.com>
2025-02-22 18:57:11 +0300

    Refresh token

--- springboot/src/main/java/com/canvas/springboot/utils/Constants.java
+++ springboot/src/main/java/com/canvas/springboot/utils/Constants.java
@@ -4,6 +4,6 @@ import java.nio.charset.StandardCharsets;
 
 public class Constants {
     public static final String SIGNING_KEY="SFjI6p6uYL1u3OQZEZ0t7nq5DT0Jil2vn2+2bY30bBc=";
-    public static final long JWT_TOKEN_VALIDITY_MS=10 * 60 * 60 * 1000;
+    public static final long JWT_TOKEN_VALIDITY_MS=10 * 60 * 1000;
     public static final byte[] SIGNING_KEY_BYTES = SIGNING_KEY.getBytes(StandardCharsets.UTF_8);
 }
