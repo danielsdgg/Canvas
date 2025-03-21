e075083a3d2a1f744c0a44865c72e6111ea3347e
Daniel Muiruri <gathigidg26@gmail.com>
2025-03-21 14:41:10 +0300

    changes

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -2,8 +2,8 @@ import axios from 'axios';
 
 // Create an Axios instance with the Spring Boot backend URL
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:8080", // Use Vite's env variable
-  timeout: 1000,                             // Optional: Add a timeout
+  baseURL: "https://canvas-xkxr.onrender.com/", 
+  timeout: 1000,                             
   headers: {
     'Content-Type': 'application/json',      // Set the headers, like Content-Type
   },
